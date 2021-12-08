9b39f2650d7f45e32bd0e26c2b8472a61f040bc5
Roman Zhirov <7997800@gmail.com>
2021-12-08 07:16:24 +0300

    upd brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -17,26 +17,28 @@ def main():
 
     def to_play_even_game():
         attempt = 1
-        while attempt <=3:
+        while attempt <= 3:
             question_number = get_question_number()
             print('Question: {}'.format(question_number))
-        
+
             right_answer = get_right_answer(question_number)
             user_answer = get_user_answer()
-        
+
             game_result = get_game_result(right_answer, user_answer)
 
-            if game_result == True:
+            if game_result is True:
                 print('Correct!')
-                attempt += 1 
+                attempt += 1
             else:
-                print("'{}' is wrong answer ;(. Correct answer was '{}'.".format(user_answer, right_answer))
+                print("'{}' is wrong answer ;(. Correct answer was '{}'.".
+                      format(user_answer, right_answer))
                 print("Let's try again, {}!".format(user_name))
                 return False
         return True
 
-    if to_play_even_game() == True:
+    if to_play_even_game() is True:
         print('Congratulations, {}!'.format(user_name))
+
 
 if __name__ == '__main__':
     main()
