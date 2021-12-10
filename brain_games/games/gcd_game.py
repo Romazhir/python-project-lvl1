b26de2cb9e98629f30361c3eb91132c722d54159
Roman Zhirov <7997800@gmail.com>
2021-12-10 10:52:25 +0300

    add gcd_game.py

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -5,12 +5,12 @@ def get_gcd_right_answer(question_number_1, question_number_2):
     question_numbers_list = [question_number_1, question_number_2]
     question_numbers_list.sort()
 
-    num1 = question_numbers_list[0] # меньшее число
-    num2 = question_numbers_list[1] # большее число
+    num1 = question_numbers_list[0]  # меньшее число
+    num2 = question_numbers_list[1]  # большее число
 
     if num2 % num1 == 0:
         return num1
-    
+
     divisor = num1 // 2
     while divisor > 0:
         if num1 % divisor == 0:
