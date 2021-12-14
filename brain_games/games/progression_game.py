9350f3a678aab0c9e503b8282668054a57147883
Roman Zhirov <7997800@gmail.com>
2021-12-14 23:24:21 +0300

    upd progression_game.py

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -9,7 +9,7 @@ import random
 # quantity_elements = 10
 # progression_step = random.randint(1, 99)
 
-def get_game_list():
+def get_game_list(start_number, quantity_elements, progression_step):
     step_counter = 1
     number = start_number
     progression_list = []
@@ -20,18 +20,23 @@ def get_game_list():
     return progression_list
 
 # game_list = get_game_list()
-
 # print(game_list)
+
+
+# определение индекса элемента, который будет скрыт
+def get_hidden_element():
+    return random.randint(0, 9)
 
 # формирование прогрессии для показа игроку
 # x_number = random.randint(0, 9)
 # right_answer = game_list[x_number]
 
-def get_game_list_for_player():
+
+def get_game_list_for_player(hidden_element, game_list):
     element_counter = 0
     list_for_player = []
     while element_counter <= 9:
-        if element_counter != x_number:
+        if element_counter != hidden_element:
             list_for_player.append(game_list[element_counter])
         else:
             list_for_player.append('..')
