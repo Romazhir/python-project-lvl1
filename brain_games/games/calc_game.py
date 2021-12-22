fe800e8e884dc49bca4037ebf2ed767d48aa03bc
Roman Zhirov <7997800@gmail.com>
2021-12-22 22:17:35 +0300

    upd calc_game.py

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -1,35 +1,32 @@
 # функции для brain-calc
 
-import operator
+from random import randint
 from random import choice
 
 
-# случайный выбор оператора
-def get_calc_operator():
-    operator_list = [operator.add, operator.sub, operator.mul]
-    return choice(operator_list)
+# правила игры
+def rules_calc():
+    return 'What is the result of the expression?'
 
-# print(get_calc_operator()(1,2))
+
+# случайное число
+def get_calc_question_number():
+    return randint(1, 99)
 
 
 # определение символа оператора
-def get_calc_operator_symbol(calc_operator):
-    if calc_operator == operator.add:
-        return '+'
+def get_calc_operator_symbol():
+    operations = ['+', '-', '*']
+    return choice(operations)
 
-    if calc_operator == operator.sub:
-        return '-'
 
-    if calc_operator == operator.mul:
-        return '*'
-
-# calc_operator = get_calc_operator()
-# print(calc_operator(2,3))
-# print(get_calc_operator_symbol(calc_operator))
+# строка для вопроса
+def get_calc_question_expression():
+    return '{} {} {}'.format(get_calc_question_number(),
+                             get_calc_operator_symbol(),
+                             get_calc_question_number())
 
 
 # определение правильного ответа
-def get_calc_right_answer(num1, num2, calc_operator):
-    return calc_operator(num1, num2)
-
-# print(get_calc_right_answer(2, 3, calc_operator))
+def get_calc_right_answer(expression):
+    return str(eval(expression))
