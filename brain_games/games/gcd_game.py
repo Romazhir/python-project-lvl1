68dfa2b41a673f23b8a0184b5d48e478e3eb9629
Roman Zhirov <7997800@gmail.com>
2021-12-22 23:36:58 +0300

    upd gcd_game.py

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -12,6 +12,7 @@ def rules_gcd():
 def get_gcd_question_number():
     return randint(1, 99)
 
+
 # строка для вопроса
 def get_gcd_question_string():
     return '{} {}'.format(get_gcd_question_number(),
@@ -21,14 +22,14 @@ def get_gcd_question_string():
 # определяем правильный ответ (перевод в str можно перенести в движок)
 def get_gcd_right_answer(questions_string):
     numbers_list = questions_string.split(' ')
-    
+
     first_number = int(numbers_list[0])
     second_number = int(numbers_list[1])
-    
+
     while first_number != second_number:
         if first_number > second_number:
             first_number = first_number - second_number
         else:
-            second_number = second_number - first_number        
-    
+            second_number = second_number - first_number
+
     return str(first_number)
