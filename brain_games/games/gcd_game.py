a782e8d2e944a68848bf02b5a41d8270a802f50f
Roman Zhirov <7997800@gmail.com>
2022-01-01 22:26:55 +0300

    upd gcd_game.py, upd brain_gcd.py

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -1,35 +1,28 @@
-# функции для brain-gcd
-
 from random import randint
 
 
 # правила игры
-def rules_gcd():
-    return 'Find the greatest common divisor of given numbers.'
+RULES = 'Find the greatest common divisor of given numbers.'
 
 
-# случайное число
-def get_gcd_question_number():
-    return randint(1, 99)
+# формирование вопроса и вычисление правильного ответа
+def get_games_data():
+    number_1 = randint(1, 99)
+    number_2 = randint(1, 99)
+
+    question = '{} {}'.format(number_1, number_2)
+
+    answer = get_gcd(number_1, number_2)
+
+    return [str(question), str(answer)]
 
 
-# строка для вопроса
-def get_gcd_question_string():
-    return '{} {}'.format(get_gcd_question_number(),
-                          get_gcd_question_number())
-
-
-# определяем правильный ответ (перевод в str можно перенести в движок)
-def get_gcd_right_answer(questions_string):
-    numbers_list = questions_string.split(' ')
-
-    first_number = int(numbers_list[0])
-    second_number = int(numbers_list[1])
-
-    while first_number != second_number:
-        if first_number > second_number:
-            first_number = first_number - second_number
+# функция для вычисления НОД
+def get_gcd(number_1, number_2):
+    while number_1 != number_2:
+        if number_1 > number_2:
+            number_1 = number_1 - number_2
         else:
-            second_number = second_number - first_number
+            number_2 = number_2 - number_1
 
-    return first_number
+    return number_1
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
-from brain_games.game_drive import play_game
-from brain_games.games.gcd_game import rules_gcd
-from brain_games.games.gcd_game import get_gcd_question_string
-from brain_games.games.gcd_game import get_gcd_right_answer
+from brain_games.engine import play_game
+import brain_games.games.gcd_game
 
 
 def main():
-    play_game(rules_gcd, get_gcd_question_string, get_gcd_right_answer)
+    play_game(brain_games.games.gcd_game)
 
 
 if __name__ == '__main__':
