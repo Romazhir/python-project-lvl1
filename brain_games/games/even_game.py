41dc7e41a861565d30a22b1f01d5a94b2518691f
Roman Zhirov <7997800@gmail.com>
2022-01-01 21:26:49 +0300

    upd even_game.py

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -10,13 +10,13 @@ def is_even(number):
     return (number % 2) == 0
 
 
-# формирование данных для игры
+# формирование вопроса и вычисление правильного ответа
 def get_games_data():
 
-    question_number = randint(1, 99)
+    question = randint(1, 99)
 
-    if is_even(question_number) is True:
+    if is_even(question) is True:
         right_answer = 'yes'
     else:
         right_answer = 'no'
-    return [str(question_number), str(right_answer)]
+    return [str(question), str(right_answer)]
