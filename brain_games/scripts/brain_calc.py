0fff1b1cefde36123d4b07fb7d1c6b4d3c07b10f
Roman Zhirov <7997800@gmail.com>
2022-01-01 21:28:16 +0300

    upd brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
-from brain_games.game_drive import play_game
-from brain_games.games.calc_game import rules_calc
-from brain_games.games.calc_game import get_calc_question_expression
-from brain_games.games.calc_game import get_calc_right_answer
+from brain_games.engine import play_game
+import brain_games.games.calc_game
 
 
 def main():
-    play_game(rules_calc, get_calc_question_expression, get_calc_right_answer)
+    play_game(brain_games.games.calc_game)
 
 
 if __name__ == '__main__':
