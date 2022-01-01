193e2a753201d8e8edf9036f071849d7b513869b
Roman Zhirov <7997800@gmail.com>
2022-01-01 14:04:24 +0300

    upd brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
-from brain_games.game_drive import play_game
-from brain_games.games.even_game import rules_even
-from brain_games.games.even_game import get_question_number
-from brain_games.games.even_game import get_even_right_answer
+from brain_games.engine import play_game
+import even_game
 
 
 def main():
-    play_game(rules_even, get_question_number, get_even_right_answer)
+    play_game(even_game)
 
 
 if __name__ == '__main__':
