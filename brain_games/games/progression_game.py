4cdcbc67188690ad97e18083da742cbc60c8c633
Roman Zhirov <7997800@gmail.com>
2022-01-01 23:30:43 +0300

    upd progression_game.py, upd brain_progression.py

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -1,37 +1,17 @@
-# функции для brain_progression
-
 import random
 
 
-FIRST_NUMBER_INDEX = 0
-FINITE_NUMBER_INDEX = 9
-
-
 # правила игры
-def rules_progression():
-    return 'What number is missing in the progression?'
+RULES = 'What number is missing in the progression?'
 
-
-# получаем первый элемент прогрессии
-def get_first_progression_number():
-    return random.randint(1, 99)
-
-
-# получаем разность прогрессии
-def get_common_difference():
-    return random.randint(1, 99)
-
-
-# получаем индекс скрытого элемента прогрессии
-def get_hidden_index():
-    return random.randint(FIRST_NUMBER_INDEX, FINITE_NUMBER_INDEX)
+FINITE_NUMBER_INDEX = 9
 
 
 # создаем арифметическую прогрессию
 def make_arithmetic_progression():
     counter = 0
-    progression_number = get_first_progression_number()
-    common_difference = get_common_difference()
+    progression_number = random.randint(1, 99)
+    common_difference = random.randint(1, 99)
     progression_list = []
 
     while counter <= FINITE_NUMBER_INDEX:
@@ -41,43 +21,15 @@ def make_arithmetic_progression():
     return progression_list
 
 
-# создаем прогрессию с пропущенным элементом
-def make_progression_list_for_question():
-    counter = 0
-    hidden_index = get_hidden_index()
+# формируем вопрос и правильный ответ
+def get_games_data():
     progression_list = make_arithmetic_progression()
-    list_for_question = []
+    hidden_element_index = random.randint(0, FINITE_NUMBER_INDEX)
 
-    while counter <= FINITE_NUMBER_INDEX:
-        if counter != hidden_index:
-            list_for_question.append(progression_list[counter])
-        else:
-            list_for_question.append('..')
-        counter += 1
-    return list_for_question
+    answer = progression_list.pop(hidden_element_index)
 
+    progression_list.insert(hidden_element_index, '..')
 
-# создаем строку для вопроса
-def get_progression_question():
-    return ' '.join(map(str, make_progression_list_for_question()))
+    question = ' '.join(map(str, progression_list))
 
-
-# получаем правильный ответ
-def get_progression_right_answer(question_string):
-    question_string = list(question_string.split())
-
-    if question_string.index('..') < (FINITE_NUMBER_INDEX - 2):
-        common_difference = (int(question_string[
-                             FINITE_NUMBER_INDEX]) - int(
-                             question_string[FINITE_NUMBER_INDEX - 1]))
-        progression_right_answer = (int(question_string[
-                                    question_string.index('..') + 1
-                                    ]) - common_difference)
-    else:
-        common_difference = (int(question_string[
-                             FIRST_NUMBER_INDEX + 1]) - int(
-                             question_string[FIRST_NUMBER_INDEX]))
-        progression_right_answer = (int(question_string[
-                                    question_string.index('..') - 1
-                                    ]) + common_difference)
-    return progression_right_answer
+    return [str(question), str(answer)]
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 
-from brain_games.game_drive import play_game
-from brain_games.games.progression_game import rules_progression
-from brain_games.games.progression_game import get_progression_question
-from brain_games.games.progression_game import get_progression_right_answer
+from brain_games.engine import play_game
+import brain_games.games.progression_game
 
 
 def main():
-    play_game(rules_progression,
-              get_progression_question,
-              get_progression_right_answer)
+    play_game(brain_games.games.progression_game)
 
 
 if __name__ == '__main__':
