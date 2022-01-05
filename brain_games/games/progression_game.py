04cbee0f9803622bd649c774660903ad9f029261
Roman Zhirov <7997800@gmail.com>
2022-01-05 11:20:13 +0300

    upd progression_game.py

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -1,35 +1,31 @@
 import random
 
 
-# правила игры
 RULES = 'What number is missing in the progression?'
+ROUND_NUMBERS = 3
+PROGRESSION_LENGTH = 10
 
-FINITE_NUMBER_INDEX = 9
+
+def generate_progression(first, difference, length):
+    progression = []
+
+    for index in range(0, length):
+        progression.append(first)
+        first += difference
+    return progression
 
 
-# создаем арифметическую прогрессию
-def make_arithmetic_progression():
-    counter = 0
-    progression_number = random.randint(1, 99)
+def generate_question_answer():
+    first_number = random.randint(1, 99)
     common_difference = random.randint(1, 99)
-    progression_list = []
+    hidden_element_index = random.randint(0, PROGRESSION_LENGTH - 1)
 
-    while counter <= FINITE_NUMBER_INDEX:
-        progression_list.append(progression_number)
-        progression_number += common_difference
-        counter += 1
-    return progression_list
-
-
-# формируем вопрос и правильный ответ
-def get_games_data():
-    progression_list = make_arithmetic_progression()
-    hidden_element_index = random.randint(0, FINITE_NUMBER_INDEX)
+    progression_list = generate_progression(first_number,
+                                            common_difference,
+                                            PROGRESSION_LENGTH)
 
     answer = progression_list.pop(hidden_element_index)
-
     progression_list.insert(hidden_element_index, '..')
-
     question = ' '.join(map(str, progression_list))
 
-    return [str(question), str(answer)]
+    return (str(question), str(answer))
