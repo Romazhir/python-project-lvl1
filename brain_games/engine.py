5262171afd0c95b146c5bc3343a60b7102689c23
Roman Zhirov <7997800@gmail.com>
2022-01-07 23:39:47 +0300

    upd engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,14 +1,16 @@
 import prompt
 
 
+ROUND_NUMBERS = 3
+
 def play_game(game):
     print('Welcome to the Brain Games!')
 
     user_name = prompt.string('May I have your name? ')
     print('Hello, {}!'.format(user_name))
-    print(game.RULES)
+    print(game.RULE)
 
-    for round in range(0, game.ROUND_NUMBERS):
+    for round in range(0, ROUND_NUMBERS):
         question_answer = tuple(game.generate_question_answer())
         question = question_answer[0]
         answer = question_answer[1]
